127978e9e74732343a591bca47288d52ecbcf5ea
alvestad10 <Daniel@daniels-macbook.local>
2015-12-26 14:00:29 +0100

    Add Button functionality

--- githubTest/ViewController.swift
+++ githubTest/ViewController.swift
@@ -26,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func helloWorld(sender: AnyObject) {
+        
+        print("Hello World")
+        
+        ++member.age
+        
+        
+    }
 
 }
 
